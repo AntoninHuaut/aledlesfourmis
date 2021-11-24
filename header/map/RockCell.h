ff8623a7093ce2cce41aafc49f183f4b189f66da
Quentin Fontaine <qfon.dev@gmail.com>
2021-11-24 14:41:02 +0100

    cells

---
+++ header/map/RockCell.h
@@ -0,0 +1,15 @@
+#ifndef ANT_ROCKCELL_H
+#define ANT_ROCKCELL_H
+
+#include "BoardCell.h"
+#include "../Config.h"
+
+class RockCell : public BoardCell {
+
+public:
+    RockCell(int posLength, int posHeight) : BoardCell(posLength, posHeight, Config::ROCK_MAX_ANT_ON_CELL) {};
+
+};
+
+
+#endif
---
+++ src/map/ColonyCell.cpp
@@ -0,0 +1,1 @@
+#include "../../header/map/ColonyCell.h"
