e6f8dda27d03b733ee576faa59bcc15aef82d855
Quentin Fontaine <qfon.dev@gmail.com>
2021-11-24 13:33:28 +0100

    rename

---
+++ header/Scout.h
@@ -0,0 +1,18 @@
+#ifndef ANT_SCOUT_H
+#define ANT_SCOUT_H
+
+#include "Ant.h"
+#include "AntAge.h"
+#include "Config.h"
+
+class Scout : public Ant, public AntAge {
+
+public:
+    Scout(BoardCell *currentCell) :
+            Ant(Config::DEFAULT_HOURS_BEFORE_DEATH, Config::DEFAULT_FOOD_CONSUMING_TICK, currentCell),
+            AntAge(Config::SCOOT_HOURS_BEFORE_ADULT) {};
+
+};
+
+
+#endif
