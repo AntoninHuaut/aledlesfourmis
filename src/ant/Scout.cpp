acbabe0bbf07ee1c1482f23a60baf291c1ebb0be
quentinfon <qfon.dev@gmail.com>
2021-12-01 14:16:33 +0100

    Scoot move

--- src/ant/Scout.cpp
+++ src/ant/Scout.cpp
@@ -5,5 +5,30 @@ void Scout::tickMove(Board *board) {
 
     auto *cells = getAvailableCellToMove(board);
 
+    if(cells->empty()) return;
+
+    auto *unKnownCells = new list<BoardCell *>;
+
+    for (auto const &cell: *cells) {
+        if (!cell->isVisited()){
+            unKnownCells->push_back(cell);
+        }
+    }
+
+    BoardCell *nextCell = nullptr;
+
+    if(!unKnownCells->empty()){
+        auto l_front = unKnownCells->begin();
+        std::advance(l_front, CustomRandom::randInt(0, unKnownCells->size()-1));
+        nextCell = *l_front;
+    } else {
+        auto l_front = cells->begin();
+        std::advance(l_front, CustomRandom::randInt(0, cells->size()-1));
+        nextCell = *l_front;
+    }
+
+    if(nextCell != nullptr){
+        goToCell(nextCell, true);
+    }
 
 }
