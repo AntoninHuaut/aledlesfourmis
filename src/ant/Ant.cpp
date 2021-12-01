69e78611c97cb1ccfa7f1ed3006d4ff1bf65f38b
Antonin Huaut <antonin.huaut@gmail.com>
2021-12-01 10:56:09 +0100

    refractor

--- src/ant/Ant.cpp
+++ src/ant/Ant.cpp
@@ -28,21 +28,13 @@ int Ant::getAntTileNumber() {
         int lengthDiff = currentCell->getPosLength() - lastCell->getPosLength();
         int heightDiff = currentCell->getPosHeight() - lastCell->getPosHeight();
 
-        if (lengthDiff == 1 && heightDiff == 0)
-            tile += 1;
-        else if (lengthDiff == 1 && heightDiff == 1)
-            tile += 2;
-        else if (lengthDiff == 0 && heightDiff == 1)
-            tile += 3;
-        else if (lengthDiff == -1 && heightDiff == 1)
-            tile += 4;
-        else if (lengthDiff == -1 && heightDiff == 0)
-            tile += 5;
-        else if (lengthDiff == -1 && heightDiff == -1)
-            tile += 6;
-        else if (lengthDiff == 0 && heightDiff == -1)
-            tile += 7;
-
+        if (lengthDiff == 1 && heightDiff == 0) tile += 1;
+        else if (lengthDiff == 1 && heightDiff == 1) tile += 2;
+        else if (lengthDiff == 0 && heightDiff == 1) tile += 3;
+        else if (lengthDiff == -1 && heightDiff == 1) tile += 4;
+        else if (lengthDiff == -1 && heightDiff == 0) tile += 5;
+        else if (lengthDiff == -1 && heightDiff == -1) tile += 6;
+        else if (lengthDiff == 0 && heightDiff == -1) tile += 7;
     }
 
     return tile;
