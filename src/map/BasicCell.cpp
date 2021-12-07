3d4968609a15b1fae59825ef5d33f337c566cdfa
Antonin Huaut <antonin.huaut@gmail.com>
2021-12-07 13:47:47 +0100

    max phero render

--- src/map/BasicCell.cpp
+++ src/map/BasicCell.cpp
@@ -3,19 +3,19 @@
 list<int> BasicCell::getLayersTileNumbers() {
     list<int> tiles;
 
-    if(getPheromone() > 0){
-        int maxPhero = 100;
-        int step = maxPhero / 5;
+    if (getPheromone() > 0) {
+        float maxPheromone = Config::get()->getMaxPheromoneOnCell();
+        float step = maxPheromone / 5;
 
         int toAdd = static_cast<int>(getPheromone() / step);
-        if(toAdd > 5) toAdd = 5;
+        if (toAdd > 5) toAdd = 5;
 
-        tiles.push_back(FIRST_PHEROMONE_LEVEL + (toAdd-1));
+        tiles.push_back(FIRST_PHEROMONE_LEVEL + toAdd);
     }
 
     if (foodAmount > 1000) {
         tiles.push_back(BIG_FOOD_LAYER);
-    } else if (foodAmount > 0){
+    } else if (foodAmount > 0) {
         tiles.push_back(FOOD_LAYER);
     }
 
